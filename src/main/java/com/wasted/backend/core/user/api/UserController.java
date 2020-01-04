e18a79079448e75ec51f91fbb17ae624261af26c
Beniamin Pantea <beniamin.pantea@catalysts.cc>
2020-01-04 17:27:53 +0200

    added unauthenticated endpoint for test purposes

--- src/main/java/com/wasted/backend/core/user/api/UserController.java
+++ src/main/java/com/wasted/backend/core/user/api/UserController.java
@@ -3,12 +3,16 @@ package com.wasted.backend.core.user.api;
 import com.wasted.backend.core.user.domain.User;
 import com.wasted.backend.core.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user")
@@ -36,7 +40,9 @@ public class UserController {
     }
 
     @GetMapping("/unauthenticated")
-    public String getUnauthenticated() {
-        return "Hello, World!";
+    public ResponseEntity<Map<String, String>> getUnauthenticated() {
+        Map<String, String> map = new HashMap();
+        map.put("response", "Hello, World!");
+        return ResponseEntity.ok(map);
     }
 }
