1fa4d79a9fa02b0ca61841251f071d167307fd4c
Alexandru <alexmihai906@gmail.com>
2020-01-19 22:29:48 +0200

    tested: adding an user, findAllUsers

--- src/main/java/com/wasted/backend/core/user/service/converter/UserConverter.java
+++ src/main/java/com/wasted/backend/core/user/service/converter/UserConverter.java
@@ -1,10 +1,10 @@
 package com.wasted.backend.core.user.service.converter;
 
-import com.wasted.backend.core.user.api.dtos.GoogleUserDto;
-import com.wasted.backend.core.user.domain.User;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.ReportingPolicy;
+        import com.wasted.backend.core.user.api.dtos.GoogleUserDto;
+        import com.wasted.backend.core.user.domain.User;
+        import org.mapstruct.Mapper;
+        import org.mapstruct.MappingTarget;
+        import org.mapstruct.ReportingPolicy;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserConverter {
